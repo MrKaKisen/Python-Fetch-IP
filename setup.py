9c96112a160c1570b909b48ed6d40db6dfc69521
Vilhelm Prytz <vpytkanal.sverige@gmail.com>
2017-03-31 20:58:54 +0200

    Add pypi stuff

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'ip-library',
+  packages = ['ip-library'],
+  version = '1.0',
+  description = 'Python library to get the users IP.',
+  author = 'Vilhelm Prytz',
+  author_email = 'contact@mrkakisen.net',
+  url = 'https://github.com/MrKaKisen/python-ip-library',
+  download_url = 'https://github.com/MrKaKisen/python-ip-library/archive/1.0.tar.gz',
+  keywords = ['ip', 'ip-library', 'getIP'],
+  classifiers = [],
+)
